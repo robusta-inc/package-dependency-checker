d7652b44df0cf5c43835f1e01477262ce3d4c003
SudhirR <sudhir.ravindramohan@gmail.com>
2013-08-07 22:32:53 +0530

    shuffled around

--- src/main/java/com/robusta/pdc/Main.java
+++ src/main/java/com/robusta/pdc/Main.java
@@ -1,5 +1,6 @@
 package com.robusta.pdc;
 
+import com.robusta.pdc.command.line.CommandLineParser;
 import com.robusta.pdc.command.line.ParseCommandLineArgumentHasFailed;
 import com.robusta.pdc.command.line.UserHasAskedForHelp;
 import com.robusta.pdc.domain.CommandLineArguments;
@@ -46,4 +47,20 @@ public class Main {
         }
     }
 
+    static class LoggingInterceptor {
+
+        public static final String LOG_LEVEL = "log.level";
+        public static final String DEBUG = "DEBUG";
+        public static final String INFO = "INFO";
+
+        public void interceptAndSetup(String[] args) {
+            boolean isVerbose = false;
+            for (String arg : args) {
+                if(CommandLineParser.OPTION_HYPHEN_VERBOSE.equals(arg)) {
+                    isVerbose = true;
+                }
+            }
+            System.setProperty(LOG_LEVEL, isVerbose ? DEBUG : INFO);
+        }
+    }
 }
--- src/test/java/com/robusta/pdc/LoggingInterceptorTest.java
+++ src/test/java/com/robusta/pdc/LoggingInterceptorTest.java
@@ -7,21 +7,21 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class LoggingInterceptorTest {
-    private LoggingInterceptor interceptor;
+    private Main.LoggingInterceptor interceptor;
     @Before
     public void setUp() throws Exception {
-        interceptor = new LoggingInterceptor();
+        interceptor = new Main.LoggingInterceptor();
     }
 
     @Test
     public void testInterceptAndSetup() throws Exception {
         interceptor.interceptAndSetup(new String[] {"-notVerbose"});
-        assertThat(System.getProperty(LoggingInterceptor.LOG_LEVEL), equalTo(LoggingInterceptor.INFO));
+        assertThat(System.getProperty(Main.LoggingInterceptor.LOG_LEVEL), equalTo(Main.LoggingInterceptor.INFO));
     }
 
     @Test
     public void testInterceptAndSetup_VerboseLogging() throws Exception {
         interceptor.interceptAndSetup(new String[] {"-verbose"});
-        assertThat(System.getProperty(LoggingInterceptor.LOG_LEVEL), equalTo(LoggingInterceptor.DEBUG));
+        assertThat(System.getProperty(Main.LoggingInterceptor.LOG_LEVEL), equalTo(Main.LoggingInterceptor.DEBUG));
     }
 }
